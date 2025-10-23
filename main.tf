53f614e8a76d528f435d5ff5a83dc6be0b01cef8
harikiranmp <95916935+harikiranmp@users.noreply.github.com>
2025-10-23 07:53:50 +0530

    Update NAT instance reference in route resource

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "aws_route" "private_nat_route" {
   count                  = length(module.vpc.private_route_table_ids)
   route_table_id         = module.vpc.private_route_table_ids[count.index]
   destination_cidr_block = "0.0.0.0/0"
-  instance_id            = aws_instance.nat_instance.id
+  instance_id            = aws_instance.nat_instance.primary_network_interface_id
   depends_on             = [aws_instance.nat_instance]
 }
 
