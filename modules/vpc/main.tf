61d354b4525a92f0d3e3326eae935b60b625b74b
harikiranmp <95916935+harikiranmp@users.noreply.github.com>
2025-10-23 07:48:43 +0530

    Delete output variables from main.tf
    Removed output variables for VPC resources.

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -87,22 +87,3 @@ resource "aws_route_table_association" "private_assoc" {
 # This route will be created in root module (not here),
 # but we output the private route table IDs for reference.
 # -----------------------------
-
-# -----------------------------
-# Outputs
-# -----------------------------
-output "vpc_id" {
-  value = aws_vpc.this.id
-}
-
-output "public_subnet_ids" {
-  value = [for s in aws_subnet.public : s.id]
-}
-
-output "private_subnet_ids" {
-  value = [for s in aws_subnet.private : s.id]
-}
-
-output "private_route_table_ids" {
-  value = [for rt in aws_route_table.private : rt.id]
-}
