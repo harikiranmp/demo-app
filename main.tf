a5b7892883965bd84bbf9689620c8831700c5445
harikiranmp <95916935+harikiranmp@users.noreply.github.com>
2025-10-23 07:46:45 +0530

    Remove output variables from main.tf
    Removed output variables for VPC, NAT instance, Jenkins, and EKS.

--- main.tf
+++ main.tf
@@ -106,34 +106,6 @@ module "eks" {
 }
 
 #############################################
-# Outputs
-#############################################
-
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
-output "nat_instance_id" {
-  value = aws_instance.nat_instance.id
-}
-
-output "nat_instance_public_ip" {
-  value = aws_instance.nat_instance.public_ip
-}
-
-output "jenkins_private_ip" {
-  value = module.jenkins.jenkins_private_ip
-}
-
-output "eks_cluster_name" {
-  value = module.eks.cluster_name
-}
-
-output "eks_oidc_provider" {
-  value = module.eks.oidc_provider_arn
-}
-
-#############################################
 # Public ALB for Jenkins (accessible via browser)
 #############################################
 
