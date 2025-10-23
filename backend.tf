2785c5a51607e01121c09e3709810dae702ba504
harikiranmp <95916935+harikiranmp@users.noreply.github.com>
2025-10-23 07:42:57 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-task-eks-1"
-    key            = "statefiles/sample-app/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-task-eks-locks"
-    encrypt        = true
-  }
-}
