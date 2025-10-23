a00f44cced27c32c4b10938312c4ffd8f8000408
harikiranmp <95916935+harikiranmp@users.noreply.github.com>
2025-10-23 06:02:40 +0530

    Add descriptions to VPC output variables

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,15 +1,19 @@
 output "vpc_id" {
-  value = aws_vpc.this.id
+  description = "ID of the VPC"
+  value       = aws_vpc.this.id
 }
 
 output "public_subnet_ids" {
-  value = [for s in aws_subnet.public : s.id]
+  description = "List of public subnet IDs"
+  value       = [for s in aws_subnet.public : s.id]
 }
 
 output "private_subnet_ids" {
-  value = [for s in aws_subnet.private : s.id]
+  description = "List of private subnet IDs"
+  value       = [for s in aws_subnet.private : s.id]
 }
 
 output "private_route_table_ids" {
-  value = [for rt in aws_route_table.private : rt.id]
+  description = "List of private route table IDs"
+  value       = [for rt in aws_route_table.private : rt.id]
 }
